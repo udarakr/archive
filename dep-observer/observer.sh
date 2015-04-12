fa39d4ae0a153741dfb7076dd0b74a9d45f2e72e
Udara Rathnayake <udarakr@gmail.com>
2015-04-12 13:01:08 +0530

    sending webapp deploy/undeply trigger to arduino

--- dep-observer/observer.sh
+++ dep-observer/observer.sh
@@ -3,16 +3,12 @@
 tail -fn0 /home/udara/wso2/git/wso2/fresh/product-es/modules/distribution/target/wso2es-2.0.0-SNAPSHOT/repository/logs/wso2carbon.log | while read line ; do
         echo "$line" | grep "Deployed webapp"
         if [ $? = 0 ]
-        then
-            if [[ "$line" =~ \[\/.*?] ]] ; then
-                    echo "XXXXXXXXXXXXXXXX ${BASH_REMATCH[0]}"
-            else
-                    echo "$line does not match"
-            fi
+            then
+             echo 1 > /dev/ttyUSB0 # New webapp deplyed
         fi
         echo "$line" | grep "Undeployed webapp"
-                if [ $? = 0 ]
-        then
-                echo "$line"
+        if [ $? = 0 ]
+            then
+              echo 0 > /dev/ttyUSB0 # webapp undeplyed
         fi
 done
