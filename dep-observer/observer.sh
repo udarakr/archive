b57ffae17fd5615a9d5326eb7199255c7a2cafed
Udara Rathnayake <udarakr@gmail.com>
2015-04-15 12:41:04 +0530

    adding license header to dep-observer project

--- dep-observer/observer.sh
+++ dep-observer/observer.sh
@@ -1,3 +1,18 @@
+<<LICENSE
+Copyright 2015 https://github.com/udarakr
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+LICENSE
 #!/bin/bash
 
 tail -fn0 /home/udara/wso2/git/wso2/fresh/product-es/modules/distribution/target/wso2es-2.0.0-SNAPSHOT/repository/logs/wso2carbon.log | while read line ; do
